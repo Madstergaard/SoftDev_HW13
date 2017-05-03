15b16ea84a22110047758bfd03bbdd7b74028715
Jerry Chang <jerrychang22@gmail.com>
2017-05-03 01:00:11 -0400

    Update mfr.py

--- mfr.py
+++ mfr.py
@@ -1,20 +1,24 @@
+import string
 
+with open('Treasure_Island.txt') as f:
+    text_words = (f.read()
+            .lower()
+            .strip()
+            .split())
 
-
-
-def wordfrequency(data, word):
-    l = data.split(' ')
+def wordfrequency(l, word):
     filter(lambda x: x == word, l)
     map(lambda x: 1, l)
     return reduce(lambda a, b: a + b, l)
 
 
-def totalfrequency(data, list):
-    l = data.split(' ')
+def totalfrequency(l, list):
     filter(lambda x: x in list, l)
     map(lambda x: 1, l)
     return reduce(lambda a, b: a + b, l)
 
-def mostfrequent(data):
-    l = data.split(' ')
-    
+def mostfrequent(l):
+    dict = {}
+    for e in l:
+        dict[e] = (dict[e] + 1) if e in dict.keys() else 1
+    return reduce(lambda a, b: a if dict[a] > dict[b] else b, dict.keys())
